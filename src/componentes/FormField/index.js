7e03d77df9d52a9d7abee40329bcb00abe38a30a
Camila <camiilacguimaraes@gmail.com>
2020-08-06 15:42:10 -0300

    Aula 3

---
+++ src/componentes/FormField/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function FormField({label, type, valor, name, onChange}) {
+  const fildId = `id_${name}`;
+
+  return (
+    <div>
+        <label htmlFor={fildId}>
+            {label}:
+            <input
+                id={fildId}
+                type={type}
+                value={valor}
+                name={name}
+                onChange={onChange}
+            />
+        </label>
+    </div>
+  );
+}
+
+FormField.defaultProps = {//definição automatica caso não seja preenchido
+  type: 'text',
+  value: '', //é comum o value não ter nada
+  onChange: () => {},
+};
+
+//tipar sem tipar. é important em react pq as componentes
+//precisam receber exatamente o que esperam receber
+FormField.protoType = {//olhar outros tipos na documentação
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string, //não é obrigatorio
+  valor: PropTypes.string, 
+  name: PropTypes.string.isRequired, 
+  onChange: PropTypes.func,
+};
+
+
+export default FormField;
